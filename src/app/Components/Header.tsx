c1ed9512a2fbda6f1b30f0cfac13eb591e24958d
Jonatan Helander <Jonatanhelander@hotmail.com>
2024-03-25 11:49:41 +0100

    Initial styling & layout

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -1,7 +1,13 @@
+import { HomeIcon, MagnifyingGlassIcon } from "@heroicons/react/16/solid";
+
 export default function Header() {
   return (
-    <header className="bg-neutral-900">
+    <header className="bg-neutral-900 flex">
       <h1 className="text-3xl text-red-700 font-bold p-8">Netflix</h1>
+      <div className="flex flex-1 justify-end pr-48">
+        <HomeIcon className="size-11 mt-8 mb-8 text-white"></HomeIcon>
+        <MagnifyingGlassIcon className="size-11 mt-8 mb-8 text-white flex"></MagnifyingGlassIcon>
+      </div>
     </header>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import Header from "./Components/Header";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,8 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {" "}
+      <body className="bg-neutral-700">
         <Header></Header>
         {children}
       </body>
