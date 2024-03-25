ac28521ed833f7b6ceccd7008bf33425c42433d5
Jonatan Helander <Jonatanhelander@hotmail.com>
2024-03-25 11:37:06 +0100

    Initial commit

---
+++ src/app/Components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-neutral-900">
+      <h1 className="text-3xl text-red-700 font-bold p-8">Netflix</h1>
+    </header>
+  );
+}
