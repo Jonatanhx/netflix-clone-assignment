17aec6a8a4d8836cb69766a07e7fbf9ab9a40ed9
Jonatan Helander <Jonatanhelander@hotmail.com>
2024-03-26 10:37:15 +0100

    Carousel Trending section and Recommended section added, Responsive design for carousels added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import MovieCarousel from "@/components/MovieCarousel";
+import RecommendedCarousel from "@/components/RecommendedCarousel";
+import TrendingCarousel from "@/components/TrendingCarousel";
 
 export default function Home() {
   return (
     <main>
-      <MovieCarousel></MovieCarousel>
+      <h1 className="text-2xl p-2 font-bold text-white">Trending</h1>
+      <TrendingCarousel></TrendingCarousel>
+      <h1 className="text-2xl p-2 font-bold text-white">Recommended for you</h1>
+      <RecommendedCarousel></RecommendedCarousel>
     </main>
   );
 }
---
+++ src/components/RecommendedCarousel.tsx
@@ -0,0 +1,39 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import movies from "../movies.json";
+import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
+
+export default function RecommendedCarousel() {
+  const [data, setData] = useState(movies);
+
+  return (
+    <Carousel>
+      <CarouselContent>
+        {data
+          .filter((item) => !item.isTrending)
+          .map((item, index) => (
+            <CarouselItem
+              className="sm:basis-1/4 md:basis-1/3 lg:basis-1/5 relative w-[50vh] h-[60vh] sm:h-[40vh] md:h-[50vh] lg:h-[50vh]"
+              key={index}
+            >
+              <div className="absolute inset-0">
+                <Image
+                  src={item.thumbnail}
+                  alt={item.title}
+                  quality={5}
+                  priority
+                  fill
+                ></Image>
+              </div>
+            </CarouselItem>
+          ))}
+      </CarouselContent>
+
+      {/*       <CarouselPrevious />
+      <CarouselNext /> */}
+    </Carousel>
+  );
+}
+
+//Importera carousel autoplay från embla carousel plugin?
