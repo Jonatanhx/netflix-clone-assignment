325976f29859c49860e5ebc6cb87188aa3ee7162
Jonatan Helander <Jonatanhelander@hotmail.com>
2024-03-28 14:59:14 +0100

    Added stylings and navigation to film view from search bar result

--- src/app/search/[searchTerm]/page.tsx
+++ src/app/search/[searchTerm]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import movieData from "../../../data/movieData";
@@ -23,24 +24,26 @@ export default function SearchResults() {
   return (
     <div className="container mx-auto py-8">
       <h1 className="text-2xl font-bold mb-4 text-white">
-        Search Results for {" ' " + searchTerm + " ' "}
+        Search Results for {"'" + searchTerm + "'"}
       </h1>
       {filteredMovies.length > 0 ? (
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {filteredMovies.map((movie) => (
-            <li
-              key={movie.slug}
-              className="bg-gray-800 text-white p-4 rounded-md shadow-md"
-            >
-              <Image
-                src={movie.thumbnail}
-                alt={movie.title}
-                width={640}
-                height={320}
-              />
-              <h3 className="text-lg font-bold">{movie.title}</h3>
-              <p className="text-gray-400">{movie.genre}</p>
-            </li>
+            <Link key={movie.title} href={`/movie/${movie.slug}`}>
+              <li
+                key={movie.slug}
+                className="bg-[#0A0A0A] text-white p-4 rounded-md shadow-md"
+              >
+                <Image
+                  src={movie.thumbnail}
+                  alt={movie.title}
+                  width={640}
+                  height={320}
+                />
+                <h3 className="text-lg font-bold">{movie.title}</h3>
+                <p className="text-gray-400">{movie.genre}</p>
+              </li>
+            </Link>
           ))}
         </ul>
       ) : (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,16 +7,18 @@ export default function Navbar() {
   return (
     <header className="bg-[#0A0A0A] flex">
       <Link href="/">
-        <h1 className="text-3xl text-red-700 font-bold m-8">Netflix</h1>
+        <h1 className="text-4xl md:text-5xl lg:text-6xl text-red-700 font-bold m-8">
+          Netflix
+        </h1>
       </Link>
 
       <div className="flex flex-1 justify-end pr-8 mt-8 mb-8">
         <Link href="/">
-          <HomeIcon className="size-11 text-white"></HomeIcon>
+          <HomeIcon className="size-11 mt-2 text-white"></HomeIcon>
         </Link>
 
         <Link href="/bookmarks">
-          <BookmarkPlusIcon className="size-11 text-white flex"></BookmarkPlusIcon>
+          <BookmarkPlusIcon className="size-11 mt-2 text-white flex"></BookmarkPlusIcon>
         </Link>
 
         <SearchBar></SearchBar>
