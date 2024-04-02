18d5e33f90eb87b561279a25adac1154254a3495
Jonatan Helander <Jonatanhelander@hotmail.com>
2024-04-02 10:18:01 +0200

    Added bookmark icon to search results

--- src/app/search/[searchTerm]/page.tsx
+++ src/app/search/[searchTerm]/page.tsx
@@ -1,14 +1,17 @@
 "use client";
+import BookmarkButton from "@/components/BookmarkButton";
+import { BookmarkContext } from "@/contexts/BookmarkContext";
 import Image from "next/image";
 import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import movieData from "../../../data/movieData";
 
 export default function SearchResults() {
   const [searchTerm, setSearchTerm] = useState("");
   const params = useParams();
   const router = useRouter();
+  const bookmarkContext = useContext(BookmarkContext);
 
   useEffect(() => {
     if (typeof params.searchTerm === "string") {
@@ -16,6 +19,10 @@ export default function SearchResults() {
       setSearchTerm(decodedSearchTerm);
     }
   }, [params.searchTerm]);
+  if (!bookmarkContext) {
+    return null;
+  }
+  const { bookmarkedMovies, toggleBookmark } = bookmarkContext;
 
   const filteredMovies = movieData.filter((movie) =>
     movie.title.toLowerCase().includes(String(searchTerm).toLowerCase())
@@ -29,21 +36,29 @@ export default function SearchResults() {
       {filteredMovies.length > 0 ? (
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {filteredMovies.map((movie) => (
-            <Link key={movie.title} href={`/movie/${movie.slug}`}>
-              <li
-                key={movie.slug}
-                className="bg-[#0A0A0A] text-white p-4 rounded-md shadow-md"
-              >
-                <Image
-                  src={movie.thumbnail}
-                  alt={movie.title}
-                  width={640}
-                  height={320}
+            <li
+              key={movie.slug}
+              className="bg-[#0A0A0A] text-white p-4 rounded-md shadow-md"
+            >
+              <div className="relative">
+                <Link key={movie.title} href={`/movie/${movie.slug}`}>
+                  <Image
+                    src={movie.thumbnail}
+                    alt={movie.title}
+                    width={640}
+                    height={320}
+                  />
+                </Link>
+                <BookmarkButton
+                  isBookmarked={bookmarkedMovies.some(
+                    (m) => m.slug === movie.slug
+                  )}
+                  onClick={() => toggleBookmark(movie)}
                 />
-                <h3 className="text-lg font-bold">{movie.title}</h3>
-                <p className="text-gray-400">{movie.genre}</p>
-              </li>
-            </Link>
+              </div>
+              <h3 className="text-lg font-bold">{movie.title}</h3>
+              <p className="text-gray-400">{movie.genre}</p>
+            </li>
           ))}
         </ul>
       ) : (
