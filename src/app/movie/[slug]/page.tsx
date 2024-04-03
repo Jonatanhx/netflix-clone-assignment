7a2fb6e86d72ae1fc06cc9af14019811f4ca00dc
Jonatan Helander <Jonatanhelander@hotmail.com>
2024-04-03 14:11:47 +0200

    modified how dark mode affects movie view, made it more visible and accessible

--- src/app/movie/[slug]/page.tsx
+++ src/app/movie/[slug]/page.tsx
@@ -20,11 +20,11 @@ export default function MoviePage({ params }: { params: { slug: string } }) {
 
   return (
     <main
-      className={`flex flex-col h-screen ${
-        isDarkMode ? "bg-black text-white" : "bg-white text-black"
+      className={`flex flex-col h-screen p-6 ${
+        isDarkMode ? "bg-black" : "bg-white"
       }`}
     >
-      <div className="flex flex-col sm:flex-row md:flex-row justify-center items-center flex-1">
+      <div className="flex flex-col sm:flex-row md:flex-row justify-center items-center bg-[#0A0A0A]">
         <div className="relative w-320 h-640 mr-8 sm:mr-0 sm:mb-8 md:mr-8 md:mb-0">
           <Image
             src={movie.thumbnail}
@@ -38,13 +38,13 @@ export default function MoviePage({ params }: { params: { slug: string } }) {
             onClick={() => toggleBookmark(movie)}
           />
         </div>
-        <div className="flex-1 sm:mt-0 md:mt-0 p-4">
+        <div className="flex-1 sm:mt-0 md:mt-0 p-4 text-white">
           <h2 className="text-2xl sm:text-4xl font-bold mb-4">{movie.title}</h2>
           <p className="mb-2">Released: {movie.year}</p>
           <p className="mb-2">{movie.genre}</p>
           <p className="mb-2">Rated: {movie.rating}</p>
           <p className="mb-2">Features: {movie.actors.join(", ")}</p>
-          <p className="mb-4">{movie.synopsis}</p>
+          <p className="mb-4 text-gray-400">{movie.synopsis}</p>
         </div>
         <div className="flex flex-1"></div>
       </div>
